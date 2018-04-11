76754a301f2ed8bb54598d7380a5b91a5bf4a98c
Anton Loskutov <antonloskutov@yandex.ru>
2018-04-11 17:02:14 +0000

    Fixed leak memory in task 6

--- include/afina/coroutine/Engine.h
+++ include/afina/coroutine/Engine.h
@@ -127,6 +127,7 @@ public:
         }
 
         // Shutdown runtime
+        delete []std::get<0>(idle_ctx->Stack);
         delete idle_ctx;
         this->StackBottom = 0;
     }
@@ -173,7 +174,7 @@ public:
             // current coroutine finished, and the pointer is not relevant now
             cur_routine = nullptr;
             pc->prev = pc->next = nullptr;
-            delete std::get<0>(pc->Stack);
+            delete []std::get<0>(pc->Stack);
             delete pc;
 
             // We cannot return here, as this function "returned" once already, so here we must select some other
